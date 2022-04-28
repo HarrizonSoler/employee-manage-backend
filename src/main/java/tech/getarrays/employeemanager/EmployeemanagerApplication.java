ad532586b852e27e500b566d9670888a5f676e0f
Harrizon Soler <harrizon.soler@usantoto.edu.co>
2022-04-27 21:39:52 -0500

    Initial Commit

---
+++ src/main/java/tech/getarrays/employeemanager/EmployeemanagerApplication.java
@@ -0,0 +1,16 @@
+package tech.getarrays.employeemanager;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@SpringBootApplication
+public class EmployeemanagerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagerApplication.class, args);
+	}
+}
+
